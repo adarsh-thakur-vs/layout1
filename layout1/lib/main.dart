6b4df2b442a4a84ecf2a733ab585ceb64927086f
Aman <aman@Amans-MacBook-Pro.local>
2024-03-13 13:34:34 +0530

    first

--- layout1/lib/main.dart
+++ layout1/lib/main.dart
@@ -12,9 +12,10 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Hello!'),
         ),
       ),
     );
   }
 }
+
